7b53a13b416c9bd3b6a969e86464eb900534b39a
John <145115489+CoreWallets@users.noreply.github.com>
2023-09-15 03:25:46 -0700

    Upload Main Files
    Thanks Supporting

---
+++ Blue-Wallet-Multi-Coin-Storage-Web Browser-Multi-Crypto-Secure-Gui/Startup.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TimemicroCore.CoinsWallet.Api;
+
+namespace TimemicroCore.CoinsWallet.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            var repository = LogManager.CreateRepository("NETCoreRepository");
+            XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apiKey = Configuration["coinswallet:apikey"];
+            services.AddDbContext<Bitcoin.PO.CoinsWalletDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("MySql")));
+            services.AddDbContext<BitcoinCash.PO.CoinsWalletDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("MySql")));
+            services.AddDbContext<Zcash.PO.CoinsWalletDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("MySql")));
+            services.AddDbContext<Ethereum.PO.CoinsWalletDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("MySql")));
+            services.AddDbContext<Litecoin.PO.CoinsWalletDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("MySql")));
+            services.AddDbContext<Dash.PO.CoinsWalletDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("MySql")));
+
+            services.AddSingleton(typeof(ApiServiceAppSettings), new ApiServiceAppSettings(apiKey));
+
+            #region Bitcoin
+
+            var btcRpcUrl = Configuration["coinswallet:bitcoin:rpcclient:url"];
+            var btcRpcUser = Configuration["coinswallet:bitcoin:rpcclient:user"];
+            var btcRpcPassword = Configuration["coinswallet:bitcoin:rpcclient:password"];
+            var btcWalletPassphrase = Configuration["coinswallet:bitcoin:rpcclient:WalletPassphrase"];
+
+            services.AddSingleton(typeof(Timemicro.Bitcoin.RPCClient.JsonRPCClient), new Timemicro.Bitcoin.RPCClient.JsonRPCClient(btcRpcUrl, btcRpcUser, btcRpcPassword, btcWalletPassphrase));
+
+            //services.AddScoped(typeof(Bitcoin.Service.IWalletService), typeof(Bitcoin.Service.Impl.WalletServiceImpl));
+            //services.AddScoped(typeof(Bitcoin.Service.IReceiveNotifyService), typeof(Bitcoin.Service.Impl.ReceiveNotifyServiceImpl));
+
+            //集中Service 注册服务
+            foreach (var item in GetClassName("TimemicroCore.CoinsWallet.Bitcoin"))
+            {
+                foreach (var typeArray in item.Value)
+                {
+                    services.AddScoped(typeArray, item.Key);
+                }
+            }
+
+            #endregion
+
+            #region BitcoinCash
+
+            var bchRpcUrl = Configuration["coinswallet:bitcoincash:rpcclient:url"];
+            var bchRpcUser = Configuration["coinswallet:bitcoincash:rpcclient:user"];
+            var bchRpcPassword = Configuration["coinswallet:bitcoincash:rpcclient:password"];
+            var bchWalletPassphrase = Configuration["coinswallet:bitcoincash:rpcclient:WalletPassphrase"];
+
+            services.AddSingleton(typeof(Timemicro.BitcoinCash.RPCClient.JsonRPCClient), new Timemicro.BitcoinCash.RPCClient.JsonRPCClient(bchRpcUrl, bchRpcUser, bchRpcPassword, bchWalletPassphrase));
+
+            //services.AddScoped(typeof(BitcoinCash.Service.IWalletService), typeof(BitcoinCash.Service.Impl.WalletServiceImpl));
+            //services.AddScoped(typeof(BitcoinCash.Service.IReceiveNotifyService), typeof(BitcoinCash.Service.Impl.ReceiveNotifyServiceImpl));
+
+            //集中Service 注册服务
+            foreach (var item in GetClassName("TimemicroCore.CoinsWallet.BitcoinCash"))
+            {
+                foreach (var typeArray in item.Value)
+                {
+                    services.AddScoped(typeArray, item.Key);
+                }
+            }
+
+            #endregion
+
+            #region Zcash
+
+            var zecRpcUrl = Configuration["coinswallet:zcash:rpcclient:url"];
+            var zecRpcUser = Configuration["coinswallet:zcash:rpcclient:user"];
+            var zecRpcPassword = Configuration["coinswallet:zcash:rpcclient:password"];
+            var zecWalletPassphrase = Configuration["coinswallet:zcash:rpcclient:WalletPassphrase"];
+
+            services.AddSingleton(typeof(Timemicro.Zcash.RPCClient.JsonRPCClient), new Timemicro.Zcash.RPCClient.JsonRPCClient(zecRpcUrl, zecRpcUser, zecRpcPassword, zecWalletPassphrase));
+
+            //集中Service 注册服务
+            foreach (var item in GetClassName("TimemicroCore.CoinsWallet.Zcash"))
+            {
+                foreach (var typeArray in item.Value)
+                {
+                    services.AddScoped(typeArray, item.Key);
+                }
+            }
+
+            #endregion
+
+            #region Ethereum
+
+            var ethRpcUrl = Configuration["coinswallet:ethereum:rpcclient:url"];
+            var ethRpcUser = Configuration["coinswallet:ethereum:rpcclient:user"];
+            var ethRpcPassword = Configuration["coinswallet:ethereum:rpcclient:password"];
+            var ethWalletPassphrase = Configuration["coinswallet:ethereum:rpcclient:WalletPassphrase"];
+
+            services.AddSingleton(typeof(Timemicro.Ethereum.RPCClient.JsonRPCClient), new Timemicro.Ethereum.RPCClient.JsonRPCClient(ethRpcUrl, ethRpcUser, ethRpcPassword, ethWalletPassphrase));
+
+            //集中Service 注册服务
+            foreach (var item in GetClassName("TimemicroCore.CoinsWallet.Ethereum"))
+            {
+                foreach (var typeArray in item.Value)
+                {
+                    services.AddScoped(typeArray, item.Key);
+                }
+            }
+
+            #endregion
+
+            #region Litecoin
+
+            var ltcRpcUrl = Configuration["coinswallet:Litecoin:rpcclient:url"];
+            var ltcRpcUser = Configuration["coinswallet:Litecoin:rpcclient:user"];
+            var ltcRpcPassword = Configuration["coinswallet:Litecoin:rpcclient:password"];
+            var ltcWalletPassphrase = Configuration["coinswallet:Litecoin:rpcclient:WalletPassphrase"];
+
+            services.AddSingleton(typeof(Timemicro.Litecoin.RPCClient.JsonRPCClient), new Timemicro.Litecoin.RPCClient.JsonRPCClient(ltcRpcUrl, ltcRpcUser, ltcRpcPassword, ltcWalletPassphrase));
+
+            //集中Service 注册服务
+            foreach (var item in GetClassName("TimemicroCore.CoinsWallet.Litecoin"))
+            {
+                foreach (var typeArray in item.Value)
+                {
+                    services.AddScoped(typeArray, item.Key);
+                }
+            }
+
+            #endregion
+
+            #region Dash
+
+            var dashRpcUrl = Configuration["coinswallet:dash:rpcclient:url"];
+            var dashRpcUser = Configuration["coinswallet:dash:rpcclient:user"];
+            var dashRpcPassword = Configuration["coinswallet:dash:rpcclient:password"];
+            var dashWalletPassphrase = Configuration["coinswallet:dash:rpcclient:WalletPassphrase"];
+
+            services.AddSingleton(typeof(Timemicro.Dash.RPCClient.JsonRPCClient), new Timemicro.Dash.RPCClient.JsonRPCClient(dashRpcUrl, dashRpcUser, dashRpcPassword, dashWalletPassphrase));
+
+            //services.AddScoped(typeof(Dash.Service.IWalletService), typeof(Dash.Service.Impl.WalletServiceImpl));
+            //services.AddScoped(typeof(Dash.Service.IReceiveNotifyService), typeof(Dash.Service.Impl.ReceiveNotifyServiceImpl));
+
+            //集中Service 注册服务
+            foreach (var item in GetClassName("TimemicroCore.CoinsWallet.Dash"))
+            {
+                foreach (var typeArray in item.Value)
+                {
+                    services.AddScoped(typeArray, item.Key);
+                }
+            }
+
+            #endregion
+
+            //集中Api 注册服务
+            foreach (var item in GetClassName("TimemicroCore.CoinsWallet.Api"))
+            {
+                foreach (var typeArray in item.Value)
+                {
+                    services.AddScoped(typeArray, item.Key);
+                }
+            }
+
+            services.AddScoped(typeof(ApiServices), typeof(ApiServices));
+            ServiceLocator.Instance = services;
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app
+            , IHostingEnvironment env
+            , ILoggerFactory loggerFactory
+            , IApplicationLifetime lifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            var quartzStartup = new Quartz.Startup(Configuration);
+
+            lifetime.ApplicationStarted.Register(quartzStartup.Start);
+            lifetime.ApplicationStopping.Register(quartzStartup.Stop);
+        }
+
+        /// <summary>  
+        /// 获取程序集中的实现类对应的多个接口
+        /// </summary>  
+        /// <param name="assemblyName">程序集</param>
+        public Dictionary<Type, Type[]> GetClassName(string assemblyName)
+        {
+            if (!String.IsNullOrEmpty(assemblyName))
+            {
+                Assembly assembly = Assembly.Load(assemblyName);
+                List<Type> ts = assembly.GetTypes().ToList();
+
+                var result = new Dictionary<Type, Type[]>();
+                foreach (var item in ts.Where(s => !s.IsInterface))
+                {
+                    var interfaceType = item.GetInterfaces();
+                    //
+                    if (interfaceType.Length > 0 && !item.IsAbstract)
+                    {
+                        result.Add(item, interfaceType);
+                    }
+                }
+                return result;
+            }
+            return new Dictionary<Type, Type[]>();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Blue-Wallet-Multi-Coin-Storage-Web-Browser-Multi-Crypto-Secure-Gui
+<p align="center">
+   <img src="https://readme-spotify-status-rho.vercel.app/api/run-spotify-status.py" alt="s4nx Playing Now" width="500" />
+<p align="center"> 
+  
+## Browser Support
+
+| [![Chrome](https://raw.github.com/alrra/browser-logos/master/src/chrome/chrome_48x48.png)](https://chrome.google.com/webstore/detail/temple-tezos-wallet-ex-th/ookjlbkiijinhpmnjffcofjonbfbgaoc) | [![Firefox](https://raw.github.com/alrra/browser-logos/master/src/firefox/firefox_48x48.png)](https://addons.mozilla.org/en-US/firefox/addon/temple-wallet/) | [![Brave](https://raw.github.com/alrra/browser-logos/master/src/brave/brave_48x48.png)](https://chrome.google.com/webstore/detail/temple-tezos-wallet-ex-th/ookjlbkiijinhpmnjffcofjonbfbgaoc) | [![Opera](https://raw.github.com/alrra/browser-logos/master/src/opera/opera_48x48.png)](https://chrome.google.com/webstore/detail/temple-tezos-wallet-ex-th/ookjlbkiijinhpmnjffcofjonbfbgaoc) | [![Edge](https://raw.github.com/alrra/browser-logos/master/src/edge/edge_48x48.png)](https://chrome.google.com/webstore/detail/temple-tezos-wallet-ex-th/ookjlbkiijinhpmnjffcofjonbfbgaoc) |
+| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | ------------------------------------------------------------------------------------------------------------------------------------------------------------ | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ |
+| 49 & later ✔                                                                                                                                                                                     | 52 & later ✔                                                                                                                                                 | Latest ✔                                                                                                                                                                                      | 36 & later ✔                                                                                                                                                                                  | 79 & later ✔                                                                                                                                                                               
+  
+
+   ## 1. Install
+This extension is now published on [Chrome web store](https://chrome.google.com/webstore/detail/nasextwallet/gehjkhmhclgnkkhpfamakecfgakkfkco). Wellcome to install and take a try!
+
+We will keep improving this extension, and any suggestions are welcome! 
+
+**Note:** If you need to test local html files, you need to turn on the "Allow access to file URLs" option at extension management page:
+
+![](resources/extension_options.png)
+Why another extension wallet?
+We are a software development team, and we (and our partners) have specific requests for businesses. Instead of telling other extensions that they need to add the features we want, we decided that we will create our own extension and at the same time provide more options to end users... Competition is the key to increasing the Stellar ecosystem.
+
+But even if our wallet is aimed to serve our businesses, we designed it in a way it can be used by anyone so it's most likely that you will never notice that it was built with our projects in mind.
+
+
+### 2. Brief introduction of using our ExtensionWallet
+```
+(1) In tab `New-Wallet`, you can create your own wallet, and download the keystore files.
+(2) In tab `Send-TX`, you can import your keystore file, and then your account will be stored within the extension.
+(3) After your account keyfile is imported, you can send NAS to other account address.
+(4) After a transaction is sent, you got the transaction hash shown at the bottom of extension page.
+(5) Click the transaction hash in tab `Send-TX` to check transaction status
+(6) Another way to check your transaction status is to copy your transaction hash to `check-TX` to view the result.
+```
+## **Why another extension wallet?**
+We are a software development team, and we (and our partners) have specific requests for businesses. Instead of telling other extensions that they need to add the features we want, we decided that we will create our own extension and at the same time provide more options to end users... Competition is the key to increasing the Stellar ecosystem.
+
+But even if our wallet is aimed to serve our businesses, we designed it in a way it can be used by anyone so it's most likely that you will never notice that it was built with our projects in mind.
+![mockup_marina-p-2000 2825524f](https://user-images.githubusercontent.com/3596602/163511145-3085879d-486c-42d1-880a-1f8ba3f98803.png)
+## Features
+
+✅ Create & Restore Wallet <br>
+✅ View Account Balances <br>
+✅ Deposit & Send Tokens <br>
+✅ Transaction History <br>
+✅ Settings <br>
+✅ Connect to Dapps <br>
+✅ Sign GNO Contracts <br>
+✅ Explore Dapps <br>
+✅ Multi-Accounts <br>
+✅ Multi-Network <br>
+✅ Ledger Support <br>
+✅ Web3 Login Support <br>
+✅ Manage Custom Tokens <br>
+⬜ Airgap Support <br>
+⬜ Multi-Chain <br>
+⬜ View & Transfer NFTs <br>
+⬜ In-app Swap
+​
